ff58750eef53964568003969912967e35e5ae8b1
Yongjie Xue <yxue9@huskers.unl.edu>
2023-11-27 08:12:50 -0600

    MongoDB findOne by ObjectID

--- pages/customers/[id].tsx
+++ pages/customers/[id].tsx
@@ -3,59 +3,57 @@ import { ParsedUrlQuery } from "querystring";
 import { useRouter } from "next/router";
 import { Customer } from "./index";
 import axios, { AxiosError } from "axios";
-import { MongoClient } from "mongodb";
+import { BSONType, MongoClient, ObjectId } from "mongodb";
+import clientPromise from "../../lib/mongodb";
 import { ServerApiVersion } from "mongodb";
+import { BSONError } from 'bson'
 type Props = {
     customer?: Customer;
 }
 
 interface Params extends ParsedUrlQuery {
     id: string
-
 }
-// const uri = "mongodb+srv://xue:<password>@xue.hjvsy80.mongodb.net/?retryWrites=true&w=majority";
-// const {  ServerApiVersion } = require('mongodb');
-// const client = new MongoClient(uri, {
-//     serverApi: {
-//         version: ServerApiVersion.v1,
-//         strict: true,
-//         deprecationErrors: true,
-//     }
-// });
+
 
 
 export const getStaticProps: GetStaticProps<Props, Params> = async (context) => {
-    const uri = "mongodb+srv://xue:123@xue.hjvsy80.mongodb.net/Customers?retryWrites=true&w=majority";
 
-    const mongoClient = new MongoClient(uri);
-    const data = await mongoClient.db()
-        .collection("Customers")
-        .find()
-        .toArray();
-
-    console.log("!!!!!", data);
-
-    // request for the data - >api
     const params = context.params!;
+
     try {
-        const result = await axios.get<{ customer: Customer }>(`http://127.0.0.1:8000/api/customers/${context?.params?.id}`)
-        // console.log("result", result)
+        const mongoClient = await clientPromise;
+
+        const data = await mongoClient.db()
+            .collection("Customers")
+            .findOne({ "_id": new ObjectId(params.id) }) as Customer
+        console.log("!!data!!", data)
+        if (!data) {
+            return {
+                notFound: true,
+                revalidate: 60
+            };
+        }
         return {
             props: {
-                customer: result.data.customer
+                customer: JSON.parse(JSON.stringify(data))
             },
             revalidate: 60,
         }
     } catch (err) {
-        if (err instanceof AxiosError) {
-            return {
-                notFound: true,
-                revalidate: 60,
-            }
-        }
-        return {
-            props: {},
-        }
+
+        // print()
+        console.log("print err", err);
+        // if (err instanceof BSONError) {
+
+        //     return {
+        //         notFound: true,
+        //     }
+        // }
+        // return {
+        //     props: {},
+        // }
+        throw err;
 
     }
 }
@@ -89,38 +87,10 @@ const Customer: NextPage<Props> = (props) => {
     }
     const { id } = router.query;
     console.log(id)
-    return <h1>Customer {props.customer ? props.customer.name : null} </h1>;
+    return <h1> {props.customer ? ("Customer" + props.customer.name) : null} </h1>;
 };
 
 export default Customer;
 
 
 
-
-////
-
-
-// const uri = "mongodb+srv://xue:<password>@xue.hjvsy80.mongodb.net/?retryWrites=true&w=majority";
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//     serverApi: {
-//         version: ServerApiVersion.v1,
-//         strict: true,
-//         deprecationErrors: true,
-//     }
-// });
-
-// async function run() {
-//     try {
-//         // Connect the client to the server	(optional starting in v4.7)
-//         await client.connect();
-//         // Send a ping to confirm a successful connection
-//         await client.db("admin").command({ ping: 1 });
-//         console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//     } finally {
-//         // Ensures that the client will close when you finish/error
-//         await client.close();
-//     }
-// }
-// run().catch(console.dir);
--- pages/customers/index.tsx
+++ pages/customers/index.tsx
@@ -1,17 +1,14 @@
 import { NextPage, GetStaticProps, InferGetStaticPropsType } from "next";
 import axios from 'axios';
 import { MongoClient } from "mongodb";
-import { ServerApiVersion } from "mongodb";
+import { ServerApiVersion, ObjectId } from "mongodb";
+import clientPromise from "../../lib/mongodb";
 export type Customer = {
-    id: number,
+    _id: ObjectId,
     name: string,
     industry: string
 }
-type Order = {
-    id: number,
-    description: string,
-    totalInCents: number
-}
+
 
 type GetCustomerReponse = {
     customers: Customer[]
@@ -22,27 +19,28 @@ export const getStaticProps: GetStaticProps = (async (context) => {
     // By returning { props: { posts } }, the Blog component
 
 
-    const uri = "mongodb+srv://xue:123@xue.hjvsy80.mongodb.net/Customers?retryWrites=true&w=majority";
-    // const uri = "mongodb+srv://xue:123@xue.hjvsy80.mongodb.net/sample_mflix?retryWrites=true&w=majority";
+    // const uri = "mongodb+srv://xue:123@xue.hjvsy80.mongodb.net/Customers?retryWrites=true&w=majority";
+    // // const uri = "mongodb+srv://xue:123@xue.hjvsy80.mongodb.net/sample_mflix?retryWrites=true&w=majority";
+    // const mongoClient = new MongoClient(uri);
 
+    const mongoClient = await clientPromise;
 
-    const mongoClient = new MongoClient(uri);
     const data = await mongoClient.db()
         .collection("Customers")
         .find()
         .toArray();
 
-    console.log("!!!!!", data);
+    // console.log("!!!!!", data);
 
     // will receive `posts` as a prop at build time
-    const result = await axios.get<{
-        customers: Customer[]
-    }>("http://127.0.0.1:8000/api/customers/");
-    // console.log("result", result)
+    // const result = await axios.get<{
+    //     customers: Customer[]
+    // }>("http://127.0.0.1:8000/api/customers/");
+    // // console.log("result", result)
 
     return {
         props: {
-            customers: result.data.customers,
+            customers: JSON.parse(JSON.stringify(data)),
         },
         revalidate: 20,
 
@@ -60,9 +58,9 @@ const Customers: NextPage = ({ customers }: InferGetStaticPropsType<typeof getSt
         </h1>
         {customers.map((customer: Customer) => {
             return (
-                <div key={customer.id}>
+                <div key={customer._id.toString()}>
                     <p>    {customer.name}</p>
-                    <p>    {customer.id}</p>
+                    <p>    {customer._id.toString()}</p>
                     <p>    {customer.industry}</p>
                 </div>
             )
